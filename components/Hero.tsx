ffd2256496c560cc153a54c41d8374cad6c74162
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-06-17 18:48:38 +0400

    Initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="flex-1 pt-36 padding-x">
+<h1 className="hero__title">
+Find, book, or rent a car - quickly an easily
+</h1>
+<p className="hero__subtitle">
+  
+</p>
+      </div>
+      </div>
+  )
+}
+
+export default Hero
