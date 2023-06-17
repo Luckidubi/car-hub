d71494b5c93479d23b0bedb7ec81b728a0a1d36b
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-06-17 21:19:28 +0400

    Add Footer and Navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image'
 import CustomButton from './CustomButton'
 
 const Hero = () => {
@@ -22,7 +23,10 @@ handleClick={handleScroll}
 />
       </div>
       <div className="hero__image-container">
-        
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className='object-contain'/>
+        </div>
+          <div className="hero__image-overlay"/>
       </div>
       </div>
   )
--- types/index.ts
+++ types/index.ts
@@ -2,5 +2,6 @@ import {MouseEventHandler} from "react"
 export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
+    btnType: "button" | "submit";
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
